fc6c17c39dc2a15cff721767dd3b032bf3e7c0c8
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-08-20 01:01:26 +0000

    BHF

---
+++ app/views/departamentos/relacaoTodosDepartamentos.php
@@ -0,0 +1,34 @@
+<?php
+
+include '../../controllers/departamentoController.php';
+ 
+?>
+<h1>Relat&oacute;rio de Departamentos</h1>
+<table class="aatable">
+	<tr>
+		<th>Nome</th>
+		<th>Sigla</th>
+		<th>Centro</th>
+	</tr>
+<?php
+	$departamento = new DepartamentoController();
+	$departamentos = $departamento->getDepartamentos();
+	foreach ( $departamentos as $key =>$value ) {
+		echo "<tr>";
+		echo "<td>";
+		echo utf8_encode( $value['nome'] );
+		echo "</td>";
+		echo "<td>";
+		echo $value['departamento_sigla'];
+		echo "</td>";
+		echo "<td>";
+		echo $value['centro_sigla'];
+		echo "</td>";
+		echo "</tr>";
+	}
+	
+?>
+</table>
+<div id="note">
+	<p>Emitido em: <?php echo date('d/m/Y H:i:s P') ?></p>
+</div>
