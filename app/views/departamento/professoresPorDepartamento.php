f8afe33ace0dfd3b548253c3031a95b2ecf5440d
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-08-20 00:41:12 +0000

    BHF

---
+++ app/views/departamento/professoresPorDepartamento.php
@@ -0,0 +1,29 @@
+<?php
+	include '../../models/professor.php'; 
+	$professor = new Professor();
+	$professores = $professor->getProfessoresPorDepartamento( $_POST['idDepartamento'] );
+
+	if ( count( $professores ) > 0 ) {
+		echo "<table class=\"aatable\">";
+		echo "<tr>";
+		echo "<th>Nome</th>";
+		echo "<th>Sobrenome</th>";
+		echo "</tr>";
+	
+		foreach ( $professores as $professor ) {
+			echo "<tr>";
+			echo "<td>";
+			echo utf8_encode( $professor->getNome() );
+			echo "</td>";
+			echo "<td align='center'>";
+			echo utf8_encode( $professor->getSobrenome() );
+			echo "</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+	} else {
+		echo "<h2>Nenhum professor cadastrado.</h2>";
+	}
+	
+	
+?>
