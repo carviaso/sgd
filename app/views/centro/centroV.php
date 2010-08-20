935592fde5c00341362c5a49de0461b858e8fa04
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-08-20 00:29:34 +0000

    BHF

---
+++ app/views/centro/centroV.php
@@ -0,0 +1,42 @@
+<?php
+
+class CentroV {
+	
+	function CentroV() {}
+	
+	function viewCentro( $centros ) {
+		$smarty = new Smarty();
+		$smarty->template_dir = 'views/centro/templates/';
+		$smarty->compile_dir  = '../tmp/templates_c/';
+		$smarty->cache_dir    = '../tmp/cache/';
+		$smarty->config_dir   = 'views/configs/';
+		$smarty->assign( "centros", $centros );
+		$smarty->assign( "emissao", date('d/m/Y H:i:s P') );
+		$smarty->display('centro.tpl');
+	}
+	
+	function listCentros( $centros ) {
+		$smarty = new Smarty();
+		$smarty->template_dir = 'views/centro/templates/';
+		$smarty->compile_dir  = '../tmp/templates_c/';
+		$smarty->cache_dir    = '../tmp/cache/';
+		$smarty->config_dir   = 'views/configs/';
+		$smarty->assign( "centros", $centros );
+		$smarty->assign( "option", 'listCentros' );
+		$smarty->display('diretoresPorCentro.tpl');
+	}
+
+	function viewDiretoresPorCentros( $diretores ) {
+		$smarty = new Smarty();
+		$smarty->template_dir = 'views/centro/templates/';
+		$smarty->compile_dir  = '../tmp/templates_c/';
+		$smarty->cache_dir    = '../tmp/cache/';
+		$smarty->config_dir   = 'views/configs/';
+		$smarty->assign( "diretores", $diretores );
+		$smarty->assign( "option", 'diretoresPorCentro' );
+		$smarty->assign( "emissao", date('d/m/Y H:i:s P') );
+		$smarty->display('diretoresPorCentro.tpl');
+	}
+}
+
+?>
--- app/views/menuProfessor.php
+++ app/views/menuProfessor.php
@@ -1,23 +1,9 @@
 <li id="submenu">
-    <h2>Cadastros do Professor</h2>
-    <ul>
-        <li><a href="javascript:void(0);" id="cadProfessor">Professor</a></li>
-        <li><a href="javascript:void(0);" id="cadRegimeTrabalhoProfessor">Regime de Trabalho do Professor</a></li>
-        <li><a href="javascript:void(0);" id="cadAfastamentoProfessor">Afastamento do Professor</a></li>
-        <li><a href="javascript:void(0);" id="cadProgressaoFuncionalProfessor">Progress&atilde;o Funcional do Professor</a></li>
-    </ul>
+	<h2>Cadastros do Professor</h2>
+	<ul>
+		<li><a href="javascript:void(0);" id="cadProfessor">Professor</a></li>
+		<li><a href="javascript:void(0);" id="cadRegimeTrabalhoProfessor">Regime de Trabalho do Professor</a></li>
+		<li><a href="javascript:void(0);" id="cadAfastamentoProfessor">Afastamento do Professor</a></li>
+		<li><a href="javascript:void(0);" id="cadProgressaoFuncionalProfessor">Progress&atilde;o Funcional do Professor</a></li>
+	</ul>
 </li>
-<script type="text/javascript">
-	$("#cadProfessor").click(function() {
-		$('#content').load('app/cad/professor.php');
-	});
-	$("#cadRegimeTrabalhoProfessor").click(function() {
-		$('#content').load('app/cad/regimeTrabalhoProfessor.php');
-	});	
-	$("#cadAfastamentoProfessor").click(function() {
-		$('#content').load('app/cad/afastamentoProfessor.php');
-	});
-	$("#cadProgressaoFuncionalProfessor").click(function() {
-		$('#content').load('app/cad/progressaoFuncionalProfessor.php');
-	});
-</script>
