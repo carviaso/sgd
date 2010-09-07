806b8b67b5175d0984834c0dfd85d3f04c9209ab
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-09-07 00:03:31 +0000

    BHF

---
+++ app/views/pais/paisV.php
@@ -0,0 +1,17 @@
+<?php
+
+class PaisV {
+
+	function PaisV() {}
+
+	function printFormCadPais() {
+		$smarty = new Smarty();
+		$smarty->template_dir = 'views/pais/templates/';
+		$smarty->compile_dir  = '../tmp/templates_c/';
+		$smarty->cache_dir    = '../tmp/cache/';
+		$smarty->config_dir   = 'views/configs/';
+		$smarty->display('pais.tpl');
+	}
+}
+
+?>
