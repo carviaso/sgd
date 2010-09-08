077ba0f6d9213a35b3317e9b4ef2b9112e09119f
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-09-08 02:50:34 +0000

    BHF - Cadastro de categoria funcional

--- trunk/app/models/categoriaFuncional.php
+++ trunk/app/models/categoriaFuncional.php
@@ -3,6 +3,31 @@
 class CategoriaFuncional {
 
 	/**
+	 * Realiza o cadastro de uma nova Categoria Funcional
+	 *
+	 * @return stdClass
+	 */
+	public function cadastrar( $descricao ) {
+		$conexao = Conexao::con();
+		$return = new stdClass();
+
+		$descricao = utf8_decode( $descricao );
+
+		$sql[] = "INSERT INTO categoria_funcional ( descricao )";
+		$sql[] = "VALUES (";
+		$sql[] = "'{$descricao}'";
+		$sql[] = ")";
+
+		if ( mysqli_query( $conexao, join( ' ', $sql ) ) ) {
+			$return->result = 1;
+		} else {
+			$return->result = 0;
+			$return->error = mysqli_error( $conexao );
+		}
+		return $return;
+	}
+
+	/**
 	 * Retorna um array com todos os objetos Categoria Funcional
 	 *
 	 * @return array
@@ -10,10 +35,10 @@ class CategoriaFuncional {
 	function getCategoriaFuncional() {
 		$conexao = Conexao::con();
 		$categoriasFuncionais = array();
-		
+
 		$sql = "SELECT * FROM categoria_funcional ORDER BY descricao";
 		$query = mysqli_query( $conexao, $sql );
-		
+
 		while ( $row = mysqli_fetch_array( $query ) ) {
 			$categoriaFuncional = new stdClass;
 			$categoriaFuncional->idCategoriaFuncional = utf8_encode( $row['id_categoria_funcional'] );
