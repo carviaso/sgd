4d1343ad91b8b329022c0eb42c85677f14bcb44c
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-09-10 23:45:37 +0000

    BHF - Branch versao 1.0.0

--- app/views/footer.php
+++
@@ -1,8 +0,0 @@
-<div id="footer">
-    <p id="legal">
-        Copyright &copy; <?=date('Y'); ?> - UFSC - Universidade Federal de Santa Catarina.
-        Todos os direitos reservados.</p>
-    <p id="links">
-        <a href="#">Pol&iacute;tica de Privacidade</a> |
-        <a href="#">Termos de Uso</a></p>
-</div>
