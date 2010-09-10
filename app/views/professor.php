6611ca2488fe76e7b90424c3878b7da940a9bdc2
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-09-10 23:54:50 +0000

    BHF - Branch versao 1.0.0

--- app/views/professor.php
+++
@@ -1,4 +0,0 @@
-<h1>Professores</h1>
-<p>&Aacute;rea de cadastros de professores do sistema.</p>
-<p>Alunos:<br />Leandro Gobbo (06138025)<br />Aloysio Nandi Tiscoski (06138002)</p>
-<p>Bom trabalho a todos!</p>
