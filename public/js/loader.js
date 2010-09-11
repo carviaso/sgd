335623405f426e5aa14278122b8c53a984f182e7
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-09-11 23:34:18 +0000

    BHF

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,31 +1,40 @@
 var principal = {
 	loadNavCadastro: function() {
 		$(".navCadastro").click(function() {
+			gb.processing();
 			$('#content').load('app/views/cadastro.php');
 			$('#sidebar').load('app/views/menuCadastro.php', function() {
 				cadastros.loadMenu();
+				gb.processingClose();
 			});
 		});
 	},
 	loadNavProfessor: function() {
 		$(".navProfessor").click(function() {
+			gb.processing();
 			$('#content').load('app/views/professor.php');
 			$('#sidebar').load('app/views/menuProfessor.php', function() {
 				professores.loadMenu();
+				gb.processingClose();
 			});
 		});
 	},
 	loadNavRelatorio: function() {
 		$(".navRelatorio").click(function() {
+			gb.processing();
 			$('#content').load('app/views/relatorio.php');
 			$('#sidebar').load('app/views/menuRelatorio.php', function() {
 				relatorios.loadMenu();
+				gb.processingClose();
 			});
 		});
 	},
 	loadNavSobre: function() {
 		$(".navSobre").click(function() {
-			$('#content').load('app/views/sobre.php');
+			gb.processing();
+			$('#content').load('app/views/sobre.php', function() {
+				gb.processingClose();
+			});
 		});
 	}
 };
