5b6ef86602e5389e473d3b27663ef719ec953e9e
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-09-11 23:34:18 +0000

    BHF

--- trunk/public/js/relatorios.js
+++ trunk/public/js/relatorios.js
@@ -1,14 +1,21 @@
 var relatorios = {
 	loadMenu: function() {
 		$("#relCentros").click(function() {
+			gb.processing();
 			var params = { "action":"relCentros" };
-			$('#content').load("app/frontController.php", params );
+			$('#content').load("app/frontController.php", params, function(){
+				gb.processingClose();
+			});
 		});
 		$("#relDepartamentos").click(function() {
+			gb.processing();
 			var params = { "action":"relDepartamentos" };
-			$('#content').load("app/frontController.php", params );
+			$('#content').load("app/frontController.php", params, function(){
+				gb.processingClose();
+			} );
 		});
 		$("#relDiretoresCentros").click(function() {
+			gb.processing();
 			var params = { "action":"relDiretoresCentros" };
 			$('#content').load("app/frontController.php", params, function() {
 				$("#selectCentros").change(function() {
@@ -16,9 +23,11 @@ var relatorios = {
 					var params = { "action":"relDiretorPorCentro", 'idCentro': idCentro };
 					$('#departamentosPorCentro').load("app/frontController.php", params);
 				}).selectmenu({width: '100%', menuWidth: 200, maxHeight: 150, style:'popup'});
+				gb.processingClose();
 			});
 		});
 		$("#relProfessoresDepartamento").click(function() {
+			gb.processing();
 			var params = { "action":"relProfessoresDepartamento" };
 			$('#content').load("app/frontController.php", params, function() {
 				$("#selectDepartamentos").change(function() {
@@ -26,9 +35,11 @@ var relatorios = {
 					var params = { "action":"relProfessoresPorDepartamento", 'idDepartamento': idDepartamento };
 					$('#professoresPorDepartamento').load("app/frontController.php", params);
 				}).selectmenu({width: '100%', menuWidth: 200, maxHeight: 150, style:'popup'});
+				gb.processingClose();
 			});
 		});
 		$("#relDepartamentoCentro").click(function() {
+			gb.processing();
 			var params = { "action":"relDepartamentoCentro" };
 			$('#content').load("app/frontController.php", params, function() {
 				$("#selectCentros").change(function() {
@@ -36,11 +47,14 @@ var relatorios = {
 					var params = { "action":"relDepartamentoPorCentro", 'idCentro': idCentro };
 					$('#departamentosPorCentro').load("app/frontController.php", params);
 				}).selectmenu({width: '100%', menuWidth: 200, maxHeight: 150, style:'popup'});
+				gb.processingClose();
 			});
 		});
 		$("#listarProfessores").click(function() {
+			gb.processing();
 			var params = { "action":"listarProfessores" };
 			$('#content').load("app/frontController.php", params, function() {
+				gb.processingClose();
 			} );
 		});
 	}
