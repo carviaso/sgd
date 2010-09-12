90b2233aade78cd3142ede03140b210e1a66f4e7
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-09-12 17:41:24 +0000

    BHF - Menu formularios

---
+++ app/views/menuFormulario.php
@@ -0,0 +1,8 @@
+<ul>
+	<li id="submenu">
+		<h2>Formul&aacute;rios</h2>
+		<ul>
+			<li><a href="javascript:void(0);" id="avaliacaoDesempenho">Avalia&ccedil;&atilde;o de Desempenho</a></li>
+		</ul>
+	</li>
+</ul>
