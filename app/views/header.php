05eb133e67df5fd6ba58413e16afdb626e38db6e
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-09-12 17:45:06 +0000

    BHF - Menu formularios

--- app/views/header.php
+++ app/views/header.php
@@ -7,7 +7,8 @@
         <ul>
             <li><a href="javascript:void(0);" class="navCadastro" >Cadastros</a></li>
             <li><a href="javascript:void(0);" class="navProfessor" >Professor</a></li>
-            <li><a href="javascript:void(0);" class="navRelatorio" >Relat&oacute;rio</a></li>
+            <li><a href="javascript:void(0);" class="navRelatorio" >Relat&oacute;rios</a></li>
+            <li><a href="javascript:void(0);" class="navFormulario" >Formul&aacute;rios</a></li>
             <li><a href="javascript:void(0);" class="navSobre" >Sobre</a></li>
         </ul>
     </div>
