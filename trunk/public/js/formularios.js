38aaa0590b3a3af7955dcb6779ef8a144fc709f5
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-09-13 03:53:19 +0000

    BHF - Menu formularios

--- trunk/public/js/formularios.js
+++ trunk/public/js/formularios.js
@@ -3,7 +3,15 @@ var formularios = {
 		$("#cadAvaliacaoDesempenho").click(function() {
 			gb.processing();
 			var params = { "action":"printFormCadAvaliacaoDesempenho" };
-			$('#content').load("app/frontController.php", params, function(){
+			$('#content').load("app/frontController.php", params, function() {
+				$('table tr td .nota')
+					.attr({'maxlength':2})
+					.blur( function() {
+						if ( $(this).val() != '' ) {
+							alert($(this).val());
+						}
+					})
+					.parent().css({"text-align":"center"});
 				gb.processingClose();
 			});
 		});
