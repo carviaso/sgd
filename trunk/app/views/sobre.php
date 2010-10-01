7d82f2f1c56b568311db79d5ea270d5f54dd339c
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-10-01 04:04:59 +0000

    BHF

--- trunk/app/views/sobre.php
+++ trunk/app/views/sobre.php
@@ -1,5 +1,5 @@
 <h1>Seja bem vindos!</h1>
 <p>Este &eacute; um sistema desenvolvido para Gest&atilde;o das Informa&ccedil;es pertinentes aos Docentes da UFSC cujo objetivo &eacute; agilizar o processo de busca de Informa&ccedil;&eacute;es efetuada pricinpalmente pela <i>CPPD</i>.</p>
 <p>Apesar de ser totalmente funcional, esta &eacute; uma vers&atilde;o beta a ser testada e possivelmente melhorada.</p>
-<p>Desenvolvido por :<br />Leandro Gobbo<br />&amp;<br />Aloysio Nandi Tiscoski</p>
+<p>Desenvolvido por :<br />Bruno Henrique Ferronato</p>
 <p>Alterado por Fernando Augusto da Silva Cruz!</p>
