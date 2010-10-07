0fe4b949b7903724a34a1d77ba1572dce53f12b3
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-10-07 05:33:39 +0000

    BHF

--- tmp/templates_c/9ac6da322a83d6e159506eb669f0bd919d15942b.file.regTrabProfessor.tpl.php
+++ tmp/templates_c/9ac6da322a83d6e159506eb669f0bd919d15942b.file.regTrabProfessor.tpl.php
@@ -1,22 +1,23 @@
-<?php /* Smarty version 3.0rc1, created on 2010-09-23 04:58:11
+<?php /* Smarty version 3.0rc1, created on 2010-10-07 05:10:57
          compiled from "views/professor/templates/regTrabProfessor.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:264544c9ade63b1bb11-43263315%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:43324cad56616dde42-47437389%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     '9ac6da322a83d6e159506eb669f0bd919d15942b' => 
     array (
       0 => 'views/professor/templates/regTrabProfessor.tpl',
-      1 => 1285216606,
+      1 => 1286428247,
     ),
   ),
-  'nocache_hash' => '264544c9ade63b1bb11-43263315',
+  'nocache_hash' => '43324cad56616dde42-47437389',
   'function' => 
   array (
   ),
   'has_nocache_code' => false,
 )); /*/%%SmartyHeaderCode%%*/?>
-<h1>Cadastro de Regime de Trabalho do Professor</h1>	
+<h1>Cadastro de Altera&ccedil;&atilde;o de Regime de Trabalho</h1>
+	
 <div>Professor</div>
 <select id="idProfessor" class="select ui-corner-all width100">
 	<?php  $_smarty_tpl->tpl_vars['professor'] = new Smarty_Variable;
