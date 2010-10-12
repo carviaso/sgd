e7f94182728e1be02d19ae2cc8f399163f5b6560
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-10-12 15:30:26 +0000

    BHF - Alteracao nome da classe de situacao para situacaoM.

--- trunk/app/models/situacaoM.php
+++ trunk/app/models/situacaoM.php
@@ -1,6 +1,6 @@
 <?php
 
-class Situacao {
+class SituacaoM {
 
 	/**
 	 * Retorna um array com todos os objetos Situacao
@@ -10,10 +10,10 @@ class Situacao {
 	function getSituacoes() {
 		$conexao = Conexao::con();
 		$situacoes = array();
-		
+
 		$sql = "SELECT * FROM situacao ORDER BY descricao_situacao";
 		$query = mysqli_query( $conexao, $sql );
-		
+
 		while ( $row = mysqli_fetch_array( $query ) ) {
 			$situacao = new stdClass;
 			$situacao->idSituacao = utf8_encode( $row['id_situacao'] );
