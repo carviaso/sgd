49e4c16db83126d60575bd4dce0ea6d16feaa1b6
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-10-19 03:32:58 +0000

    BHF

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,4 +1,4 @@
 <div id="footer"></div>
 <div id="copyRight">
-	<div>Copyright &copy; <?php echo date('Y'); ?> - UFSC - Universidade Federal de Santa Catarina. Todos os direitos reservados.</div>
+	<div>&copy; <?php echo date('Y'); ?> - UFSC - Universidade Federal de Santa Catarina. CPPD/SGD.</div>
 </div>
