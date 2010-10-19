73f9eb1ba5993c7724c184d281fc6186279b9fd1
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-10-19 03:32:58 +0000

    BHF

--- trunk/app/views/menuLogin.php
+++ trunk/app/views/menuLogin.php
@@ -3,11 +3,11 @@
 	<div class="boxLoginSub ui-corner-all" class="ui-corner-all">
 		<div class="label">Siape:</div>
 		<div>
-			<input type="text" id="siape" value="1159414" maxlength="11" class="input ui-corner-all" />
+			<input type="text" id="siape" value="" maxlength="11" class="input ui-corner-all" />
 		</div>
 		<div class="label">Senha:</div>
 		<div>
-			<input type="password" id="senha" value="123456" class="input ui-corner-all" />
+			<input type="password" id="senha" value="" class="input ui-corner-all" />
 		</div>
 		<br />
 		<div><input type="button" class="button" id="entrar" maxlength="32" value="Entrar" /></div>
