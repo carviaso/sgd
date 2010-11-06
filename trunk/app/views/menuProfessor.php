00f721dee2178984cef2a35e1d38c757e89f9f88
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-11-06 22:14:43 +0000

    FASC - Alteraçãode rótulo Regime de Trabalho

--- trunk/app/views/menuProfessor.php
+++ trunk/app/views/menuProfessor.php
@@ -3,7 +3,7 @@
 		<h2>Cadastros do Professor</h2>
 		<ul>
 			<li><a href="javascript:void(0);" id="cadProfessor">Professor</a></li>
-			<li><a href="javascript:void(0);" id="cadRegimeTrabalhoProfessor">Regime de Trabalho do Professor</a></li>
+			<li><a href="javascript:void(0);" id="cadRegimeTrabalhoProfessor">Altera&ccedil;&atilde;o de Regime de Trabalho do Professor</a></li>
 			<li><a href="javascript:void(0);" id="cadAfastamentoProfessor">Afastamento do Professor</a></li>
 			<li><a href="javascript:void(0);" id="cadProgressaoFuncionalProfessor">Progress&atilde;o Funcional do Professor</a></li>
 		</ul>
