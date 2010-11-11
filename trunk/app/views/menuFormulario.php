4db614351b4fde2985713e3ccb8fc91d24d9d210
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-11-11 20:31:56 +0000

    FASC - Alteraçãode do Rótulo Formulários

--- trunk/app/views/menuFormulario.php
+++ trunk/app/views/menuFormulario.php
@@ -2,7 +2,7 @@
 	<li id="submenu">
 		<h2>Formul&aacute;rios</h2>
 		<ul>
-			<li><a href="javascript:void(0);" id="cadAvaliacaoDesempenho">Avalia&ccedil;&atilde;o de Desempenho</a></li>
+			<li><a href="javascript:void(0);" id="cadAvaliacaoDesempenho">Tabela de Avalia&ccedil;&atilde;o de Desempenho Aplicada a Carreira do Ensino B&aacute;sico T&eacute;cnico e Tecnol&oacute;gico</a></li>
 		</ul>
 	</li>
 </ul>
