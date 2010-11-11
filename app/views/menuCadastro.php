4e32b139de653c072c0d4f816c510b841abbf1af
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-11-11 20:24:09 +0000

    FASC - Alteraçãode do Rótulo do Menu - Correção

--- app/views/menuCadastro.php
+++ app/views/menuCadastro.php
@@ -18,7 +18,7 @@
 			<li><a href="javascript:void(0);" id="cadTipoTitulacao">Tipos de Titula&ccedil;&otilde;es</a></li>
 			<li><a href="javascript:void(0);" id="cadCategoriaFuncional">Nova Categoria Funcional</a></li>
 			<li><a href="javascript:void(0);" id="cadRegimeTrabalho">Novo Regime de Trabalho</a></li>
-			<li><a href="javascript:void(0);" id="cadPortaria">Portaria associada aos Professores</a></li>
+			<li><a href="javascript:void(0);" id="cadPortaria">Portaria Associada aos Professores</a></li>
 		</ul>
 	</li>
 </ul>
