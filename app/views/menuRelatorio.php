444f8eef50769297c5c7c4392d9299f1583f6514
bferronato <bferronato@6154eb46-5731-48cc-eaf9-7dcc24869093>
2010-11-11 20:38:58 +0000

    FASC - Alteraçãode do Rótulo Relatórios

--- app/views/menuRelatorio.php
+++ app/views/menuRelatorio.php
@@ -2,24 +2,24 @@
 	<li id="submenu">
 		<h2>Relat&oacute;rios B&aacute;sicos</h2>
 		<ul>
-			<li><a href="javascript:void(0);" id="relCentros">Centros</a></li>
+			<li><a href="javascript:void(0);" id="relCentros">Listagem dos Centros</a></li>
 			<li><a href="javascript:void(0);" id="relDiretoresCentros">Diretores dos Centros</a></li>
-			<li><a href="javascript:void(0);" id="relDepartamentos">Departamentos</a></li>
+			<li><a href="javascript:void(0);" id="relDepartamentos">Listagem de Todos os Departamentos</a></li>
 			<li><a href="javascript:void(0);" id="relChefesDepartamento">Chefes dos Departamentos</a></li>
-			<li><a href="javascript:void(0);" id="listarProfessores">Professores</a></li>
+			<li><a href="javascript:void(0);" id="listarProfessores">Listagem de Todos os Professores</a></li>
 		</ul>
 		<br />
 		<h2>Relat&oacute;rios do Professor</h2>
 		<ul>
-			<li><a href="javascript:void(0);" id="relGeralProfessor">Relat&oacute;rio Geral do Professor</a></li>
-			<li><a href="javascript:void(0);" id="relAfastamentoAposentadoria">Afastamento/Aposentadoria</a></li>
+			<li><a href="javascript:void(0);" id="relGeralProfessor">Relat&oacute;rio Geral de Cada Professor</a></li>
+			<li><a href="javascript:void(0);" id="relAfastamentoAposentadoria">Professores Afastados ou Previs&atilde;o de Aposentadoria</a></li>
 			<li><a href="javascript:void(0);" id="relProfessoresDepartamento">Professores por Departamento</a></li>
 		</ul>
 		<br />
 		<h2>Relat&oacute;rios Espec&iacute;ficos</h2>
 		<ul>
 			<li><a href="javascript:void(0);" id="relDepartamentoCentro">Departamentos por Centro</a></li>
-			<li><a href="javascript:void(0);" id="relAposentados">Professores Aposentados</a></li>
+			<li><a href="javascript:void(0);" id="relAposentados">Listagem dos Professores Aposentados</a></li>
 		</ul>
 	</li>
 </ul>
